ba0a080688250654ddfdf76ee4ed82cbbdc6d07f
deannate00 <deannate00@gmail.com>
2023-08-27 14:49:23 -0400

    Test

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //Check mark
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Github Test")
         }
         .padding()
     }
